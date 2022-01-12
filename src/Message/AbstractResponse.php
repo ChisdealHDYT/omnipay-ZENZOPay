7fcce76a602eb238010ee12a84e71be4b710d3ee
Grant Robinson <Chisdeal2014@hotmail.com>
2022-01-12 23:16:34 +0000

    Update AbstractResponse.php

--- src/Message/AbstractResponse.php
+++ src/Message/AbstractResponse.php
@@ -1,24 +1,10 @@
 <?php
-/**
- * CheckoutCom Response
- */
 
 namespace Omnipay\CheckoutCom\Message;
 
-/**
- * CheckoutCom Response
- *
- * This is the response class for all CheckoutCom requests.
- *
- * @see \Omnipay\CheckoutCom\Gateway
- */
 class AbstractResponse extends \Omnipay\Common\Message\AbstractResponse
 {
-    /**
-     * Get a token, for createCard requests.
-     *
-     * @return string|null
-     */
+
     public function getTransactionReference()
     {
         if (isset($this->data['id'])) {
@@ -29,13 +15,6 @@ class AbstractResponse extends \Omnipay\Common\Message\AbstractResponse
 
     }
 
-    /**
-     * Get the error message from the response.
-     *
-     * Returns null if the request was successful.
-     *
-     * @return string|null
-     */
     public function getMessage()
     {
         if (!$this->isSuccessful() && isset($this->data['errorCode'])) {
@@ -45,11 +24,6 @@ class AbstractResponse extends \Omnipay\Common\Message\AbstractResponse
         return null;
     }
 
-    /**
-     * Is the transaction successful?
-     *
-     * @return bool
-     */
     public function isSuccessful()
     {
         return !isset($this->data['errorCode']);
