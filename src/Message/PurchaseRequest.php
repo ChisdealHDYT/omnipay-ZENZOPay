f0c49ddd99c31530f7cc56d3bd9d89d2a59a7b60
Grant Robinson <Chisdeal2014@hotmail.com>
2022-01-12 23:19:25 +0000

    Update PurchaseRequest.php

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -1,46 +1,17 @@
 <?php
-/**
- * CheckoutCom Purchase Request
- */
 
 namespace Omnipay\ZENZOPay\Message;
 
-/**
- * ZENZOPay Purchase Request
- *
- * 
- */
 class PurchaseRequest extends AbstractRequest
 {
     public function getData()
     {
-        $this->validate('amount', 'currency');
+        $this->validate('address', 'amount');
 
         $data = array();
-        $data['value'] = $this->getAmountInteger();
-        $data['currency'] = strtoupper($this->getCurrency());
-        $data['description'] = $this->getDescription();
-        $data['metadata'] = $this->getMetadata();
-
-        if ($udf = $this->getUdfValues()) {
-            $data['udf1'] = $udf[0];
-            $data['udf2'] = isset($udf[1]) ? $udf[1] : null;
-            $data['udf3'] = isset($udf[2]) ? $udf[2] : null;
-            $data['udf4'] = isset($udf[3]) ? $udf[3] : null;
-            $data['udf5'] = isset($udf[4]) ? $udf[4] : null;
-        }
-
-//        if ($this->getCardReference()) {
-//            $data['customer'] = $this->getCardReference();
-//        } elseif ($this->getTransactionReference()) {
-//            $data['card'] = $this->getTransactionReference();
-//        } elseif ($this->getCard()) {
-//            $data['card'] = $this->getCardData();
-//        } else {
-//            // one of cardReference, token, or card is required
-//            $this->validate('card');
-//        }
-
+        $data['address'] = $this->getAddress();
+        $data['amount'] = $this->getAmount();
+        
         return $data;
     }
 
@@ -50,9 +21,5 @@ class PurchaseRequest extends AbstractRequest
 
         return $this->response = new PurchaseResponse($this, $httpResponse->json());
     }
-
-    public function getEndpoint()
-    {
-        return parent::getEndpoint() . '/tokens/payment';
-    }
+    
 }
